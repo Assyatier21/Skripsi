b61ab1ac9f282e0d95c0998bae144ce34f2f4630
Muhammad Sholeh <60766389+Assyatier21@users.noreply.github.com>
2021-12-27 16:21:03 +0700

    Add Form
    Form GUI Main

--- SimilaritySourceCodeAST-RO/src/similarity/source/code/AST/RO/MainProgram.java
+++ SimilaritySourceCodeAST-RO/src/similarity/source/code/AST/RO/MainProgram.java
@@ -8,83 +8,49 @@ public class MainProgram
 {
     public static void main(String[] args) throws Exception
     {
+        
         String mainDoc =
-        "Day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']\n" +
-        "num = int(input(\"Insert num of Day = \"))\n" +
+        "import java.util.Scanner;\n" +
+        "public class testfile {\n" +
         "\n" +
-        "if (num >= 1) and (num <= 7):\n" +
-        "    print 'The Day number is ' + str(num) + 'and the Name is ' +  hari[bil - 1]\n" +
-        "else:\n" +
-        "    print 'The day is not available'";
+        "	public static void main(String[] args) {\n" +
+        "\n" +
+        "for( int j=1; j<=7; j++) {\n" +
+        "			System.out.println(\"\");\n" +
+        "			for(int i=1; i<=j; i++) {\n" +
+        "				System.out.print(\"*\");\n" +
+        "			}\n" +
+        "			\n" +
+        "		}\n" +
+        "}\n" +
+        "}";
        
         String duplicateDoc =
-        "hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']\n" +
-        "\n" +
-        "bil = int(input(\"Masukkan angka untuk menentukan hari = \"))\n" +
-        "\n" +
-        "if (bil >= 1) and (bil <= 7):\n" +
-        "    print 'Hari ke ', bil, ' adalah hari ', hari[bil - 1]\n" +
-        "else:\n" +
-        "    print 'Hari tidak tersedia'";
-        
-//        String mainDoc =
-//        "import java.util.Scanner;\n" +
-//        "public class testfile {\n" +
-//        "\n" +
-//        "	public static void main(String[] args) {\n" +
-//        "\n" +
-//        "for( int j=1; j<=7; j++) {\n" +
-//        "			System.out.println(\"\");\n" +
-//        "			for(int i=1; i<=j; i++) {\n" +
-//        "				System.out.print(\"*\");\n" +
-//        "			}\n" +
-//        "			\n" +
-//        "		}\n" +
-//        "}\n" +
-//        "}";
-//       
-//        String duplicateDoc =
-//        "package com.company;\n" +
-//        "import java.util.Scanner;\n" +
-//        "public class law{\n" +
-//        "	public static void main(String[ args) {\n" +
-//        "		System.out.print(\"Masukkan angka : \");\n" +
-//        "		Scanner ss = new Scanner(System.in);\n" +
-//        "			Int l = ss.nextInt();\n" +
-//        "			for( int k = 1; k <= 1; k++) {\n" +
-//        "				System.out.println(\"\");\n" +
-//        "				for(int i = 1; I<=k;i++) {\n" +
-//        "				System.out.print(\"*\");\n" +
-//        "				}\n" +
-//        "			}\n" +
-//        "	}\n" +
-//        "}";
+        "package com.company;\n" +
+        "import java.util.Scanner;\n" +
+        "public class law{\n" +
+        "	public static void main(String[ args) {\n" +
+        "		System.out.print(\"Masukkan angka : \");\n" +
+        "		Scanner ss = new Scanner(System.in);\n" +
+        "			Int l = ss.nextInt();\n" +
+        "			for( int k = 1; k <= 1; k++) {\n" +
+        "				System.out.println(\"\");\n" +
+        "				for(int i = 1; I<=k;i++) {\n" +
+        "				System.out.print(\"*\");\n" +
+        "				}\n" +
+        "			}\n" +
+        "	}\n" +
+        "}";
         
         try
         {
-//            prepText process = new prepText();
-//            String mainDocFiltered = process.ppTextwithAST(mainDoc);
-//            String duplicateDocFiltered = process.ppTextwithAST(duplicateDoc);
-//
-//            tokenizeCode next = new tokenizeCode();
-//            String mainDocTokenized = next.tokenizerJava(mainDocFiltered);
-//            String duplicateDocTokenized = next.tokenizerJava(duplicateDocFiltered);
-//
-//            System.out.println("Main Doc Token :\n" + mainDocTokenized);
-//            System.out.println("");
-//            System.out.println("Duplicate Doc Token:\n" + duplicateDocTokenized);
-//            System.out.println("");
-//            ROalgorithm algorithm = new ROalgorithm();
-//            double percentage = algorithm.similarity(mainDocTokenized, duplicateDocTokenized);
-//            System.out.println("Percentage of Similarity : " + percentage);
-            
             prepText process = new prepText();
             String mainDocFiltered = process.ppTextwithAST(mainDoc);
             String duplicateDocFiltered = process.ppTextwithAST(duplicateDoc);
 
             tokenizeCode next = new tokenizeCode();
-            String mainDocTokenized = next.tokenizerPython(mainDocFiltered);
-            String duplicateDocTokenized = next.tokenizerPython(duplicateDocFiltered);
+            String mainDocTokenized = next.tokenizerJava(mainDocFiltered);
+            String duplicateDocTokenized = next.tokenizerJava(duplicateDocFiltered);
 
             System.out.println("Main Doc Token :\n" + mainDocTokenized);
             System.out.println("");
@@ -93,6 +59,8 @@ public class MainProgram
             ROalgorithm algorithm = new ROalgorithm();
             double percentage = algorithm.similarity(mainDocTokenized, duplicateDocTokenized);
             System.out.println("Percentage of Similarity : " + percentage);
+            
+            
             
         } 
         catch (Exception e)
